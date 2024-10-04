b471df094c8676156004b06b6177695814d54949
SumanBind <Sumangic0001@gmail.com>
2024-10-04 11:48:19 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,18 @@
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int cost=500, sall=700 ;
+    if(cost<sall)
+    {
+    printf("profite =%d",sall-cost);
+    }
+    else
+    {
+        printf("loss =%d",cost-sall);
+    }
+    return 0;
+}
