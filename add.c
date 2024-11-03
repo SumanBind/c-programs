9e755aca2f3c4c2b52acfa62911fc74d81d00010
SumanBind <Sumangic0001@gmail.com>
2024-11-03 09:19:24 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main() 
+{
+    float n1, n2,add;
+    printf("Enter the first number: ");
+    scanf("%f", &n1);
+
+    printf("Enter the second number: ");
+    scanf("%f", &n2);
+
+    printf("You entered: %f and %f\n", n1, n2);
+    //add=n1+n2;
+    printf("Add two numbers = %f",n1+n2);
+
+}
